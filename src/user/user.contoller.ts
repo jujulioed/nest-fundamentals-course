468946535a61f5656af4766e21981a2ee401e0f5
jujulioed <julioeduardon9@hotmail.com>
2025-03-10 22:42:02 -0300

    Create PUT, PATCH and DELETE in REST pattern
    Signed-off-by: jujulioed <julioeduardon9@hotmail.com>

--- src/user/user.contoller.ts
+++ src/user/user.contoller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
 
 @Controller('users')
 export class UserController {
@@ -17,4 +17,30 @@ export class UserController {
     async show(@Param() params) {
         return {user:{}, params}
     }
+
+    @Put(':id')
+    async update(@Body() body, @Param() params) {
+        return {
+            method: 'put',
+            body,
+            params
+        }
+    }
+
+    @Patch(':id')
+    async updatePartial(@Body() body, @Param() params) {
+        return {
+            method: 'patch',
+            body,
+            params
+        }
+    }
+
+    @Delete(':id')
+    async delete(@Param() params) {
+        return {
+            params
+        }
+    }
+
 }
